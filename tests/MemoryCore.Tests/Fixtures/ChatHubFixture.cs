576bceb86192d56acf9753c3660cff64711b97b6
Kage Martin <1011970+andymartin@users.noreply.github.com>
2025-05-08 19:33:11 -0600

    test(chat): Add ChatHub test fixtures and unit tests

---
+++ tests/MemoryCore.Tests/Fixtures/ChatHubFixture.cs
@@ -0,0 +1,29 @@
+using MemoryCore.Tests.Fixtures;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+using Neo4j.Driver;
+
+namespace MemoryCore.Tests.Fixtures
+{
+    public class ChatHubFixture : WebApplicationFactory<Program>
+    {
+        private readonly Neo4jContainerFixture _neo4jFixture;
+
+        public ChatHubFixture(Neo4jContainerFixture neo4jFixture)
+        {
+            _neo4jFixture = neo4jFixture;
+        }
+
+        protected override void ConfigureWebHost(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices(services =>
+            {
+                services.RemoveAll<IDriver>();
+                services.AddSingleton<IDriver>(_ => 
+                    GraphDatabase.Driver(_neo4jFixture.GetBoltUri(), AuthTokens.Basic("neo4j", "password")));
+            });
+        }
+    }
+}
